5965f08339ad9e642d433e198e0a87d9b029c88d
aaltonenn <joona.otso.aaltonen@gmail.com>
2023-04-03 22:30:09 +0300

    tiedostojen nimiä vaihdettu loogisemmiksi

--- src/tests/chord_determination_test.py
+++ src/tests/chord_determination_test.py
@@ -1,15 +1,15 @@
 import unittest 
 from services.service import Service
 
-class TestChord_Recognition(unittest.TestCase):
+class TestChord_Determination(unittest.TestCase):
     def test_is_major_chord(self):
-        test = Service.chord_recongition("C", "E", "G")
+        test = Service.chord_determination("C", "E", "G")
         self.assertEqual(test, "this is major chord")
 
     def test_is_minor_chord(self):
-        test = Service.chord_recongition("C", "D#", "G")
+        test = Service.chord_determination("C", "D#", "G")
         self.assertEqual(test, "this is minor chord")
 
     def test_not_major_nor_minor_chord(self):
-        test = Service.chord_recongition("C", "F", "G")
+        test = Service.chord_determination("C", "F", "G")
         self.assertEqual(test, "this is not major or minor chord")
