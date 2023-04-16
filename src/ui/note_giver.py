9e5433e04b2302449510077a42ad4dd0248133ce
aaltonenn <joona.otso.aaltonen@gmail.com>
2023-04-16 14:56:44 +0300

    note giver

--- src/ui/note_giver.py
+++ src/ui/note_giver.py
@@ -2,10 +2,10 @@ from services.service import GiveNotes
 class NoteGiver:
     def give_note(self):
         while True:
-            rootnote = input("gimme a chord's rootnote or press 0 to exit").upper()
+            rootnote = input("Give a chord's rootnote (for example C) or press 0 to exit").upper()
             if rootnote == "0":
                 break
-            majorminor = input("do you want it to be major [1] or minor [2]")
+            majorminor = input("Do you want it to be major [1] or minor [2]")
             test=GiveNotes()
             print(test.give_notes(rootnote, majorminor))
         print("going back to main menu")
